11f3c513bf5de51c65ef1088a441fbbb723f81c4
parkash34 <ohm.parkash34@gmail.com>
2026-04-22 23:47:24 +0200

    Updated it

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone, ServerlessSpec
 from langchain_groq import ChatGroq
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage
 
 
 load_dotenv()
@@ -97,3 +97,73 @@ llm = ChatGroq(
     max_tokens = 500,
     api_key = api_key
 )
+
+@app.post("/ask")
+def ask_ai(query: Query):
+    query = query.query
+
+    results = vector_store.similarity_search(query, k=4)
+    context = ""
+    for doc in results:
+        source = doc.metadata.get("source", "unknown")
+        context += f"[Source: {source}]\n{doc.page_content}\n\n"
+
+    prompt = f"""You are a restaurant assistant for Bella Italia.
+    Answer the customer question using ONLY the information below.
+    If the answer is not in the information say:
+    "I don't have that information. Please call us at 123-456-7890"
+
+    Information:
+    {context}
+
+    Customer Question: {query}
+    """
+    response = llm.invoke([HumanMessage(content=prompt)])
+    return {"answer": response.content}
+
+@app.post("/sources")
+def sources_ai(query: Query):
+    query = query.query
+    results = vector_store.similarity_search(query, k=4)
+    formatted = []
+    for doc in results:
+        formatted.append({
+            "content": doc.page_content,
+            "source": doc.metadata.get("source", "unknown"),
+            "page": doc.metadata.get("page", "N/A")
+        })
+    return {"sources": formatted}
+
+@app.post("/update")
+def update_ai():
+    try:
+        pc.delete_index("bella-italia-docs")
+        print("Old index deleted")
+
+        pc.create_index(
+            name="bella-italia-docs",
+            dimension=384,
+            metric="cosine",
+            spec=ServerlessSpec(
+                cloud="aws",
+                region="us-east-1"
+            )
+        )
+        
+        print("New index created")
+
+        global vector_store
+        vector_store = PineconeVectorStore(
+            index_name="bella-italia-docs",
+            embedding=embeddings,
+            pinecone_api_key=pinecone_api_key
+        )
+
+        chunks = build_restaurant_pipeline()
+        vector_store.add_documents(chunks)
+
+        return {"message": "Documents updated successfully!"}
+    except Exception as e:
+        return {"error": f"Update failed {str(e)}"}
+
+    
